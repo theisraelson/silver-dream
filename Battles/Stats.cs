ef740db1fbb78a8d6b03b15c2276589959fe5a58
James Israelson <jamesio.israelson@gmail.com>
2020-04-24 13:22:22 -0300

    Added Stats class

---
+++ Battles/Stats.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace SilverDream.Battles
+{
+    public class Stats
+    {
+        public int Level { get; set; }
+        public int XP { get; set; }
+        public int Speed { get; set; }
+        public int Power { get; set; }
+        public int Defense { get; set; }
+        public int Finesse { get; set; }
+        public int Luck { get; set; }
+
+        public Stats()
+        {
+            Level = 0;
+            XP = 0;
+            Speed = 0;
+            Power = 0;
+            Defense = 0;
+            Finesse = 0;
+            Luck = 0;
+        }
+    }
+}
