2da2e7919042333f4ca69901168f4dbf5ecc77b1
James Israelson <jamesio.israelson@gmail.com>
2020-04-24 19:02:53 -0300

    Added MagicSkill and worked on primitive Battles

---
+++ Battles/Skills/MagicSkill.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using SilverDream.Battles.Characters;
+
+namespace SilverDream.Battles.Skills
+{
+    public class MagicSkill : Skill
+    {
+
+        public string Name { get; }
+        public Cost Cost { get; }
+        public int Attack { get; }
+
+        public MagicSkill(string name, Cost cost, int attack)
+        {
+            Name = name;
+            Cost = cost;
+            Attack = attack;
+        }
+
+        public void ApplyEffect(Character user, Character receiver)
+        {
+            Stats uStats = user.Stats;
+            Stats rStats = user.Stats;
+            double lvlDifference = uStats.Level / rStats.Level;
+            double random = getRandomMultiplier();
+
+            double effect = (5 * Math.Sqrt(uStats.Magic / rStats.Defense * Attack) * lvlDifference * random);
+
+            receiver.Stats.Hp -= effect;
+
+        }
+
+        public static double getRandomMultiplier()
+        {
+            Random r = new Random();
+            int factor = r.Next(0,10);
+
+            double dFactor = (double)factor / 100;
+
+            return dFactor + 0.95;
+        }
+
+        public void UseCost(Character user)
+        {
+            Cost.UseCost(user);
+        }
+    }
+}
